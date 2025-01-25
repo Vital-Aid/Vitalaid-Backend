be93cc9f94f068ab1b62af3cc2e9fc1f76d9ad5e
shahid <ms4811879@gmail.com>
2025-01-25 12:53:44 +0530

    login change

--- src/index.ts
+++ src/index.ts
@@ -26,6 +26,7 @@ const corsOptions = {
   origin: 'http://localhost:3000',
   methods: ['GET', 'POST', 'PUT', 'DELETE'],
   allowedHeaders: ['Content-Type', 'Authorization',"X-MongoDb-Id"],
+  credentials: true,
 };
 
  
